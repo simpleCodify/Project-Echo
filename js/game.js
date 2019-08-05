d636f5f537ef60858c6cbe756a550ec7a82288cd
SimplyGA <jonpak1987@gmail.com>
2019-08-04 17:29:02 -0700

    Added High Score Functionality! Updated UX/UI for cleaner look + layout change. Added encouragement messages function. Added Player Name function.

--- js/game.js
+++ js/game.js
@@ -14,16 +14,22 @@ var started = false;
 var level = 0;
 var difficulty = 1;
 var msec = 800;
+var playerName = $("#inlineFormInput").val();
+var top5Scores = [];
 
 
 //-- User Keypress --//
+
+document.getElementById('inlineFormInput').addEventListener("keyup", function(){
+  playerName = $("#inlineFormInput").val();
+})
 
 $(document).on("keyup", function(e) {
   if (started === false) {
     if (e.keyCode == 32) {
       $("#level-title").text("Level " + level);
-      nextSequence();
       started = true;
+      nextSequence();   
     }
   } else {
     return;
@@ -33,13 +39,10 @@ $(document).on("keyup", function(e) {
 $(document).on("keyup", function(e) {
   //-- Test for Keyboard Key Press --//
 
-  //console.log(e.keyCode);
   var audio2 = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   let userPressedKey = $(`.key[data-key="${e.keyCode}"]`).attr("id");
 
-
-  //-- logic for pressed key on keyboard being pushed into array --//
   userClickedNotes.push(userPressedKey);
 
   //if (!key) return;
@@ -75,27 +78,109 @@ function checkAnswer(currentLevel) {
     }
   } else {
     started = false ;
+    displayHighScores();
     playSound("wrong");
 
     //$("body").addClass("game-over");
     $("#level-title").text("Game Over").css("color", "red");
     $(".key").addClass("game-over");
+    $("#level-message").text(`You got to ${level} ${playerName}!`);
 
     setTimeout(function() {
       $("#level-title").css("color", "#353a3f");
       $(".key").removeClass("game-over");
-    }, 1000);
+      $("#level-message").text("");
+    }, 1500);
     restart();
   }
 }
 
+//-- Message Section --//
+
+function encouragementMessage(){
+  //var encourageLevel = level ;
+  switch(true){
+    case level == 1:
+      $("#level-message").text(`Welcome to Echo ${playerName}`);
+      break;
+    case level == 2:
+      $("#level-message").text("TIP: There is no tip.");
+      break;
+    case level == 3:
+      $("#level-message").text("Off to a good start I guess?");
+      break;
+    case level == 4:
+      $("#level-message").text("Let's speed things up.");
+      break;
+    case level == 5:
+      $("#level-message").text("Look at Me!!");
+      break;
+    case level == 6:
+      $("#level-message").text("Did I mess you up?");
+      break;
+    case level == 7:
+      $("#level-message").text(`Encouraging words just for you ${playerName}`);
+      break;
+    case level == 8:
+      $("#level-message").text(`Alright. Fine. Let's go ${playerName}`);
+      break;
+    case level <= 10 && level > 8:
+      $("#level-message").text("Ok. Is this fast enough yet?");
+      break;
+    case level <= 12 && level > 10:
+      $("#level-message").text(`Good memory ${playerName}`);
+      break;
+    case level > 12:
+      $("#level-message").text(`You're a god ${playerName}`);
+      break;
+    default:
+      $("#level-message").text(`You got this ${playerName}!`);
+  }
+
+  $("#level-title").text("Level " + level);
+  setTimeout(function() {
+    $("#level-message").text("");
+  }, 1500);
+}
+
+// --High Scores Function-- //
+function displayHighScores(){
+  highScores();
+  for (var i = 0 ; i < top5Scores.length  ; i++ ) {
+    $("." + (i+1) + "level").text(top5Scores[i][1]);
+    $("." + (i+1) + "name" ).text(top5Scores[i][0]);
+  }
+}
+
+function highScores() {
+  if ( playerName === "" ) return;
+  var tmpTopScore = [playerName, level];
+  if ( top5Scores.length == 0 ) {
+    top5Scores.push(tmpTopScore);
+    return ;
+  } 
+  for (var i = top5Scores.length - 1; i >= 0 ; i--) {
+    if (level <= top5Scores[i][1]) {
+      top5Scores.splice(i+1,0,tmpTopScore) ;
+      break;
+    }
+    else if ( i == 0 ) {
+      top5Scores.unshift(tmpTopScore);
+    }
+  }
+  if ( top5Scores.length > 5 ) top5Scores.pop();
+
+}
+
+
+
 //-- Next Sequence Function --//
 
 function nextSequence() {
-  started = true;
   userClickedNotes = [];
   level++;
-  $("#level-title").text("Level " + level);
+  encouragementMessage();
+
   var randomKey ;
   var randomNum ;
   if (difficulty % 2 == 1) {
@@ -198,3 +283,4 @@ $('#button4').click(function() {
   $('#button1,#button2,#button3').removeClass("active");
   $(this).addClass("active");
 });
+
